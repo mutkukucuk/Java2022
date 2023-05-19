1cc2fbab48982486a61bb64342c84fad95d7d876
mutkukucuk <muhammetutku@gmail.com>
2023-05-19 12:56:47 +0300

    first commit

--- Week5/Lesson/rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ Week5/Lesson/rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -1,14 +1,19 @@
 package kodlama.io.rentACar.webApi.controllers;
 
-import java.util.List;
+import java.util.List; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 import kodlama.io.rentACar.entities.concretes.Brand;
+ 
 
 @RestController //annotation
 @RequestMapping("/api/brands")
@@ -22,10 +27,15 @@ public class BrandsController {
 	}
 	
 	@GetMapping("/getall")
-	public List<Brand> getAll(){
+	public List<GetAllBrandsResponse> getAll(){
 	//IoC
 		return brandService.getAll();
 		
 	}
+	@PostMapping("/add")
+	public void add(@RequestBody() CreateBrandRequest createBrandRequest) {
+		this.brandService.add(createBrandRequest);
+	}
+	
 	
 }
